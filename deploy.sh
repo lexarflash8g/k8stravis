60519f63905eb63dc8f5c29ff7664c120f1298ae
lexarflash8g <lexarflash8g@gmail.com>
2020-12-10 01:10:35 -0800

    new

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+docker build -t lexarflash8g/multi-cient:latest -t lexarflash8g/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t lexarflash8g/multi-server:latest -t lexarflash8g/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t lexarflash8g/multi-worker:latest -t lexarflash8g/multi-worker:$SHA-f /.worker/Dockerfile ./worker
+docker push lexarflash8g/multi-client:latest
+docker push lexarflash8g/multi-server:latest
+docker push lexarflash8g/multi-worker:latest
+docker push lexarflash8g/multi-client:$SHA
+docker push lexarflash8g/multi-server:$SHA
+docker push lexarflash8g/multi-worker:$SHA
+kubectl apply -f k8s
+kubectl set image deployments/server-deployment server=lexarflash8g/multi-server:$SHA
+kubectl set image deployments/client-deployment server=lexarflash8g/multi-client:$SHA
+kubectl set image deployments/worker-deployment server=lexarflash8g/multi-worker:$SHA
+
